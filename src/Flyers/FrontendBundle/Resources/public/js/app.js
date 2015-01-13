4369178318a26b90551d8ccc32e0213e95663c5f
renatomefi <git@renatomefi.com.br>
2015-01-13 11:15:17 -0200

    Preload name refactor

--- src/Flyers/FrontendBundle/Resources/public/js/app.js
+++ src/Flyers/FrontendBundle/Resources/public/js/app.js
@@ -13,37 +13,38 @@ var sammui = angular.module('sammui', [
 ]).config(function ($routeProvider) {
     $routeProvider.when('/login', {
         templateUrl: '/bundles/frontend/partials/login.html',
+        templatePreload: true,
         controller: 'Login',
-        reloadOnSearch: false,
-        preload: true
+        reloadOnSearch: false
     });
     $routeProvider.when('/ui', {
         templateUrl: '/bundles/frontend/partials/ui.html',
+        templatePreload: true,
         controller: 'UiCtrl',
-        reloadOnSearch: false,
-        preload: true
+        reloadOnSearch: false
     });
     $routeProvider.when('/view1', {
         templateUrl: '/bundles/frontend/partials/partial1.html',
+        templatePreload: true,
         controller: 'MyCtrl1',
-        reloadOnSearch: false,
-        preload: true
+        reloadOnSearch: false
     });
     $routeProvider.when('/view2', {
         templateUrl: '/bundles/frontend/partials/partial2.html',
+        templatePreload: true,
         controller: 'MyCtrl2',
-        reloadOnSearch: false,
-        preload: true
+        reloadOnSearch: false
     });
     $routeProvider.otherwise({redirectTo: '/login'});
 });
 
+// Template pre-load
 sammui.run([
     '$route', '$templateCache', '$http', (function ($route, $templateCache, $http) {
         console.debug('templateCache: Starting caching all templates from routes')
         var url;
         for (var i in $route.routes) {
-            if ($route.routes[i].preload) {
+            if ($route.routes[i].templatePreload) {
                 if (url = $route.routes[i].templateUrl) {
                     $http.get(url, {cache: $templateCache});
                 }
