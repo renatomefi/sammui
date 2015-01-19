c514b3da0029b64c15e10fde1b9d3a636912addc
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-01-19 16:44:14 -0200

    Retrieving translations from server.

--- src/Renatomefi/TranslateBundle/Resources/public/js/translate-services.js
+++ src/Renatomefi/TranslateBundle/Resources/public/js/translate-services.js
@@ -4,31 +4,20 @@ angular.module('sammui.translateServices', ['ngResource']).
     factory('translateLoader', ['$q', '$timeout', '$resource', function ($q, $timeout, $resource) {
 
         return function (options) {
-            var deferred = $q.defer(),
-                translations;
+            var deferred = $q.defer(), translations = new Object();
 
             var Languages = $resource('/l10n/manage/langs/:lang', {lang: '@lang'});
 
             console.debug('Downloading translate');
-            var lang = Languages.get({lang:options.key}, function(){
-                console.debug(lang);
-            });
+            var lang = Languages.get({lang: options.key}, function () {
+                lang.translations.forEach(function(t){
+                    if (t) {
+                        translations[t.key] = t.value;
+                    }
+                })
 
-            if (options.key === 'en-us') {
-                translations = {
-                    "index-title-sidebar-left": 'Srsly!',
-                    "index-title-sidebar-right": 'YlsrS!'
-                };
-            } else {
-                translations = {
-                    "index-title-sidebar-left": 'Ernsthaft!',
-                    "index-title-sidebar-right": 'Tfahtsnre!'
-                };
-            }
-
-            $timeout(function () {
                 deferred.resolve(translations);
-            }, 2000);
+            });
 
             return deferred.promise;
         };
