974e37bd359b8488b6d064b1a09fbb1fcdcd22f0
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-01-21 19:36:38 -0200

    New filter to find a key inside an array of objects and better organization at translate controllers

--- src/Renatomefi/TranslateBundle/Resources/public/js/translate-app.js
+++ src/Renatomefi/TranslateBundle/Resources/public/js/translate-app.js
@@ -1,9 +1,23 @@
 'use strict'
 
 // Configuring $translateProvider
-angular.module('sammui').config(['$translateProvider', function ($translateProvider) {
+angular.module('sammui')
+    .config(['$translateProvider', function ($translateProvider) {
 
-    $translateProvider.preferredLanguage('en-us');
-    $translateProvider.useLoader('translateLoader');
+        $translateProvider.preferredLanguage('en-us');
+        $translateProvider.useLoader('translateLoader');
 
-}]);
+    }])
+    .filter('getByKey', function () {
+        return function (data, key) {
+            var result = null;
+
+            angular.forEach(data, function (item) {
+                if (key == item.key) {
+                    result = item;
+                }
+            });
+
+            return result;
+        }
+    });
--- src/Renatomefi/TranslateBundle/Resources/public/js/translate-controllers.js
+++ src/Renatomefi/TranslateBundle/Resources/public/js/translate-controllers.js
@@ -1,38 +1,68 @@
 'use strict'
 
 angular.module('sammui.translateControllers', ['ngRoute'])
+    .filter('test', function () {
+
+    })
     // Routes
     .config(function ($routeProvider) {
-        $routeProvider.when('/l10n/admin', {
+        $routeProvider.when('/l10n/admin/:lang?', {
             templateUrl: '/l10n/admin',
             templatePreload: false,
             controller: 'TranslateAdmin',
             reloadOnSearch: false
         });
     })
-    .controller('TranslateAdmin', ['$rootScope', '$scope', '$window', 'translateLangs', 'translateLangsKeys',
-        function ($rootScope, $scope, $window, translateLangs, translateLangsKeys) {
+    .controller('TranslateAdmin', ['$rootScope', '$scope', '$window', '$filter', '$routeParams', 'translateLangs', 'translateLangsKeys',
+        function ($rootScope, $scope, $window, $filter, $routeParams, translateLangs, translateLangsKeys) {
             $scope.translate = new Object();
-
-            $scope.translate.langs = translateLangs.query();
 
             $scope.translate.table = false;
 
-            $scope.langKeysTable = function (lang) {
+            $scope.translate.langs = translateLangs.query({}, function () {
+                if ($routeParams.lang) {
+                    $scope.langKeysTable($routeParams.lang);
+                }
+            });
+
+            $scope.langKeysTable = function (lang, reload) {
                 $rootScope.loading = true;
+
+                reload = reload || false;
+
+                var language = $filter('getByKey')($scope.translate.langs, lang);
+
+                if (language && reload === false) {
+                    $scope.translate.langs.keys = language.translations;
+                    $scope.translate.table = true;
+                    $rootScope.loading = false;
+                    return;
+                }
+
                 var langKeys = translateLangsKeys.query({lang: lang}, function () {
                     $scope.translate.langs.keys = langKeys;
-                    $rootScope.loading = false;
                     $scope.translate.table = true;
+                    $rootScope.loading = false;
                 });
             };
 
-            $scope.addLangKey = function() {
+            $scope.addLangKey = function () {
                 $scope.inserted = {
                     key: null,
                     value: null
                 };
                 $scope.translate.langs.keys.push($scope.inserted);
+            };
+
+            $scope.editCheck = function (translation) {
+                console.debug('onshow check');
+                if (translation.id) {
+                    console.debug('true');
+                    $scope.translateLangKeyFormEditableKey = true;
+                } else {
+                    console.debug('false');
+                    $scope.translateLangKeyFormEditableKey = false;
+                }
             };
 
             $scope.deleteLangKey = function (index) {
