2e55845a9474f51cd202c0bc8ec9d00ca92d6b40
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-01-27 10:39:25 -0200

    Some layout and organization

--- src/Renatomefi/TranslateBundle/Resources/public/js/translate-controllers.js
+++ src/Renatomefi/TranslateBundle/Resources/public/js/translate-controllers.js
@@ -20,7 +20,8 @@ angular.module('sammui.translateControllers', ['ngRoute'])
                     if ($location.search()['lang']) {
                         $rootScope.langKeysTable();
                     }
-                })
+                }),
+                'currentLang': null
             };
 
             $scope.langChange = function (lang) {
@@ -49,13 +50,15 @@ angular.module('sammui.translateControllers', ['ngRoute'])
                     function () {
                         $scope.translate.langKeys = langKeys;
                         $scope.translate.table = true;
+                        $rootScope.loading = false;
                     },
                     function (errorResponse) {
                         $scope.error = errorResponse;
                         $scope.Ui.turnOn("modalError");
+                        $rootScope.loading = false;
                     });
 
-                $rootScope.loading = false;
+
             };
 
             $scope.addLangKey = function () {
@@ -67,11 +70,7 @@ angular.module('sammui.translateControllers', ['ngRoute'])
             };
 
             $scope.editCheck = function (translation) {
-                if (translation.id) {
-                    $scope.translateLangKeyFormEditableKey = true;
-                } else {
-                    $scope.translateLangKeyFormEditableKey = false;
-                }
+                $scope.translateLangKeyFormEditableKey = (translation.id) ? true : false;
             };
 
             $rootScope.$on('errorResourceReq', function (e, errorResponse) {
@@ -80,8 +79,8 @@ angular.module('sammui.translateControllers', ['ngRoute'])
                 $scope.Ui.turnOn("modalError");
             });
 
-            $scope.saveLangKey = function (data, translation) {
-                var d = $q.defer();
+            $scope.saveLangKey = function (data, translation, index) {
+                var post = $q.defer();
                 var type = (angular.isUndefined(translation.id)) ? 'save' : 'update';
 
                 translateLangsKeys[type](
@@ -91,23 +90,30 @@ angular.module('sammui.translateControllers', ['ngRoute'])
                         value: data.value
                     },
                     function (response) {
-                        if (!angular.isUndefined(response.id)) d.resolve();
+                        if (!angular.isUndefined(response.id)) {
+                            $scope.translate.langKeys[index] = response;
+                            post.resolve();
+                        }
                     },
                     function (error) {
-                        d.reject(error.statusText);
+                        $rootScope.$emit('errorResourceReq', error);
+                        post.reject(error.statusText);
                     }
                 );
 
-                return d.promise;
+                return post.promise;
             };
 
             $scope.deleteLangKey = function (index) {
                 var langTranslation = $scope.translate.langKeys[index];
 
+                console.log('form', $scope.translateLangKeyForm);
                 if (typeof langTranslation.id === 'undefined') {
                     $scope.translate.langKeys.splice(index, 1);
                     return;
                 }
+
+                var post = $q.defer();
 
                 translateLangsKeys.delete(
                     {
@@ -117,11 +123,14 @@ angular.module('sammui.translateControllers', ['ngRoute'])
                     function (response) {
                         if (response.ok) {
                             $scope.translate.langKeys.splice(index, 1);
+                            post.resolve();
                         } else {
-                            $rootScope.$emit('errorResourceReq', function (e, response) {
-                            });
+                            $rootScope.$emit('errorResourceReq', response);
+                            post.reject(response);
                         }
                     });
+
+                return post;
             };
         }
     ]);
