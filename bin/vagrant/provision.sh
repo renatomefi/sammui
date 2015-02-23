13249b0967a7e53320284f8f08d288620a772e57
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-02-23 19:43:43 -0300

    Provision improvements

--- bin/vagrant/provision.sh
+++ bin/vagrant/provision.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo "Start ---------------------------------------"
 echo "Starting sammui installation: " $(date)
 export COMPOSER_PROCESS_TIMEOUT=900
-composer create-project renatomefidf/sammui /vagrant/sammui dev-master
+#composer create-project renatomefidf/sammui /vagrant/sammui dev-master
 sudo ln -s /vagrant/sammui/docs/nginx/vhost-sammui.conf /etc/nginx/sites-enabled/
 echo "Enabling sammui nginx virtualhost: " $([ "$?" == 0 ] && echo "ok" || echo "error")
 echo "Finishing sammui installation: " $(date)
 echo "Checking installation:"
 php /vagrant/sammui/app/check.php
+echo "Restoring MongoDB for sammui:"
+mongorestore /vagrant/sammui/docs/mongo/master/
+echo "End   ---------------------------------------"
