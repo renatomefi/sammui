f05657e088e5f550577f8452e0310602dd078171
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-02-23 20:54:06 -0300

    Keeping git repository at create-project

--- bin/vagrant/provision.sh
+++ bin/vagrant/provision.sh
@@ -3,7 +3,7 @@
 echo "Starting sammui installation: " $(date)
 echo "--------------------------------------------------------"
 export COMPOSER_PROCESS_TIMEOUT=900
-composer create-project -n --prefer-source renatomefidf/sammui /vagrant/sammui dev-master
+composer create-project -n --keep-vcs renatomefidf/sammui /vagrant/sammui dev-master
 sudo ln -s /vagrant/sammui/docs/nginx/vhost-sammui.conf /etc/nginx/sites-enabled/
 echo "Enabling sammui nginx virtualhost: " $([ "$?" == 0 ] && echo "ok" || echo "error")
 echo "Finishing sammui installation: " $(date)
