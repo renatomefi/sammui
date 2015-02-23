8c7df610571ea460ef4c93b8429e3d6535a7a50d
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-02-23 14:52:21 -0300

    Vagrant Provision shell script improved

--- bin/vagrant/provision.sh
+++ bin/vagrant/provision.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "Starting sammui installation: " $(date)
+export COMPOSER_PROCESS_TIMEOUT=900
 composer create-project renatomefidf/sammui /vagrant/sammui dev-master
-echo "Enabling sammui nginx virtualhost: " $(date)
 sudo ln -s /vagrant/sammui/docs/nginx/vhost-sammui.conf /etc/nginx/sites-enabled/
+echo "Enabling sammui nginx virtualhost: " $([ "$?" == 0 ] && echo "ok" || echo "error")
 echo "Finishing sammui installation: " $(date)
+echo "Checking installation:"
+php /vagrant/sammui/app/check.php
