3ca47e241c9e29edbc11a3e461c3497112371192
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-02-24 14:43:53 -0300

    Provision improvements

--- bin/vagrant/provision.sh
+++ bin/vagrant/provision.sh
@@ -4,6 +4,8 @@ echo "Starting sammui installation: " $(date)
 echo "--------------------------------------------------------"
 export COMPOSER_PROCESS_TIMEOUT=900
 composer create-project -n --keep-vcs renatomefidf/sammui /vagrant/sammui dev-master
+echo "Removing old and wrong vhosts:"
+sudo rm -rf /etc/nginx/sites-enabled/vhost-sammui*.conf
 sudo ln -s /vagrant/sammui/docs/nginx/vhost-sammui.conf /etc/nginx/sites-enabled/
 echo "Enabling sammui nginx virtualhost: " $([ "$?" == 0 ] && echo "ok" || echo "error")
 nginx -t
