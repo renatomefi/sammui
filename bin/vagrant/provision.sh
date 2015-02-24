ec379bc96351ebd0254abc2bdd9b184be68d980e
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-02-24 14:37:01 -0300

    nginx test

--- bin/vagrant/provision.sh
+++ bin/vagrant/provision.sh
@@ -6,6 +6,8 @@ export COMPOSER_PROCESS_TIMEOUT=900
 composer create-project -n --keep-vcs renatomefidf/sammui /vagrant/sammui dev-master
 sudo ln -s /vagrant/sammui/docs/nginx/vhost-sammui.conf /etc/nginx/sites-enabled/
 echo "Enabling sammui nginx virtualhost: " $([ "$?" == 0 ] && echo "ok" || echo "error")
+nginx -t
+echo "Testing nginx configuration: " $([ "$?" == 0 ] && echo "ok" || echo "error")
 echo "Finishing sammui installation: " $(date)
 echo "Checking installation:"
 php /vagrant/sammui/app/check.php
