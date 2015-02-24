56c5de6c24db42e86e41157fa0cbcb08f8db08be
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-02-24 09:18:13 -0300

    doc for alias

--- bin/vagrant/alias_template.sh
+++ bin/vagrant/alias_template.sh
@@ -2,6 +2,13 @@
 #Want to execute SF2 commands inside your vagrant?
 #This way you can use i.e.: $`sammui router:debug`
 
+#Easy install:
+#On your .bashrc ou the global one you can just proccess this file (or alias.sh by your needs):
+#In the end of the file:
+#. ~/SAMMUI_FOLDER/bin/alias_template.sh
+#or
+#source ~/SAMMUI_FOLDER/bin/alias_template.sh
+
 sammui_host_folder='~/Vagrant/sammui'; #Sammui folder on your pc: Usually inside Vagrant folder
 sammui_guest_folder='/vagrant/sammui'; #Sammui folder inside vagrant
 sammui_vagrant_ip='www.sammui.dev'; #Your vagrant IP
