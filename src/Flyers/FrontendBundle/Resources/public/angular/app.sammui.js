36c023d5de7c937681f1459dfe74060c3b09cc5e
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-02-26 09:22:26 -0300

    Correcting a simple merge error

--- src/Flyers/FrontendBundle/Resources/public/angular/app.sammui.js
+++ src/Flyers/FrontendBundle/Resources/public/angular/app.sammui.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on filters, and services
 var sammui = angular.module('sammui', [
     'ngRoute',
+    'ngAnimate',
     'sammui.api',
     'sammui.mainServices',
     'xeditable',
