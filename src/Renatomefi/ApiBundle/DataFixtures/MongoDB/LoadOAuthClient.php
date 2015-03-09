1fce43fffab35b41a28ef857c7b0e564fa3d5c44
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-03-09 10:32:35 -0300

    OAuth client fixture

---
+++ src/Renatomefi/ApiBundle/DataFixtures/MongoDB/LoadOAuthClient.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Renatomefi\UserBundle\DataFixtures\MongoDB;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use OAuth2\OAuth2;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class LoadOAuthClient implements FixtureInterface, ContainerAwareInterface
+{
+
+    const CLIENT_NAME = 'sammui-php-unit';
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $clientManager = $this->container->get('fos_oauth_server.client_manager.default');
+
+        $client = $clientManager->createClient();
+        $client->setName(static::CLIENT_NAME);
+        $client->setRedirectUris(['/']);
+        $client->setAllowedGrantTypes([
+            OAuth2::GRANT_TYPE_AUTH_CODE,
+            OAuth2::GRANT_TYPE_USER_CREDENTIALS,
+            OAuth2::GRANT_TYPE_REFRESH_TOKEN,
+            OAuth2::GRANT_TYPE_AUTH_CODE
+        ]);
+
+        $clientManager->updateClient($client);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 2;
+    }
+}
