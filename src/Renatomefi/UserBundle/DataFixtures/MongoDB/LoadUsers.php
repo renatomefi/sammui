3ddbce41af60e0fc193fd4be63a3d37e9a120930
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-03-09 10:06:40 -0300

    New user fixture

---
+++ src/Renatomefi/UserBundle/DataFixtures/MongoDB/LoadUsers.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Renatomefi\UserBundle\DataFixtures\MongoDB;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Renatomefi\UserBundle\Document\User;
+
+class LoadUsers implements FixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('sammui');
+        $user->setUsernameCanonical('sammui');
+        $user->setEmail('sammui-project@renatomefi.com.br');
+        $user->setEmailCanonical('sammui-project@renatomefi.com.br');
+        $user->setSalt(md5(uniqid()));
+        $user->setEnabled(true);
+        $user->setLocked(false);
+        $user->addRole(UserInterface::ROLE_SUPER_ADMIN);
+        $user->addRole('ROLE_ADMIN');
+
+        $encoder = $this->container
+            ->get('security.encoder_factory')
+            ->getEncoder($user);
+        $user->setPassword($encoder->encodePassword('sammui', $user->getSalt()));
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+}
