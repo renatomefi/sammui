6d8d8b508f3a6f44166c1610b9051122ffd7e392
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-03-09 11:26:21 -0300

    Automated tests fixs

--- bin/vagrant/test-all.sh
+++ bin/vagrant/test-all.sh
@@ -5,6 +5,7 @@ sammui_dist_folder='/home/sammui/dist'; #Sammui folder inside vagrant
 
 echo "-------- Removing Cache"
 sudo rm -rf $sammui_dist_folder/app/cache/*
+sudo rm -rf $sammui_dist_folder/app/logs/*
 echo "-------- Syncing Files"
 rsync -avh --delete --exclude app/logs --exclude app/cache --exclude .git --exclude .idea $sammui_project_folder $sammui_dist_folder
 echo "-------- Starting PHPUnit"
