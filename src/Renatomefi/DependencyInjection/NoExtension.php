f63299931ff57c1d5aa147632213258b39d08f7c
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-03-18 16:01:29 -0300

    Optimizing code climate

---
+++ src/Renatomefi/DependencyInjection/NoExtension.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Renatomefi\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class NoExtension extends Extension
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
+
+        $reflection = new \ReflectionClass($this);
+        $splFile = new \SplFileInfo($reflection->getFileName());
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator($splFile->getPath() . '/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
--- src/Renatomefi/TranslateBundle/DependencyInjection/TranslateExtension.php
+++ src/Renatomefi/TranslateBundle/DependencyInjection/TranslateExtension.php
@@ -2,10 +2,7 @@
 
 namespace Renatomefi\TranslateBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Renatomefi\DependencyInjection\NoExtension;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -14,17 +11,6 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * @codeCoverageIgnore
  */
-class TranslateExtension extends Extension
+class TranslateExtension extends NoExtension
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
-    }
 }
