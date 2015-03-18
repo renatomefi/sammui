5c55e8c2da673749fb16c6936c4870b1beafc1b9
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-03-18 16:45:10 -0300

    Optimizing code climate for mongo documents

--- src/Renatomefi/ApiBundle/Document/AuthCode.php
+++ src/Renatomefi/ApiBundle/Document/AuthCode.php
@@ -3,83 +3,12 @@
 namespace Renatomefi\ApiBundle\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
-use FOS\OAuthServerBundle\Document\AuthCode as BaseAuthCode;
-use FOS\OAuthServerBundle\Model\ClientInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Class AuthCode
  * @package Renatomefi\ApiBundle\Document
  * @ODM\Document
  */
-class AuthCode extends BaseAuthCode
+class AuthCode extends Token
 {
-    /**
-     * @ODM\Id(strategy="auto")
-     */
-    protected $id;
-
-    /**
-     * @ODM\ReferenceOne(targetDocument="Client")
-     */
-    protected $client;
-
-    /**
-     * @ODM\ReferenceOne(targetDocument="Renatomefi\UserBundle\Document\User")
-     */
-    protected $user;
-
-    /**
-     * Get id
-     *
-     * @return id $id
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set client
-     *
-     * @param Renatomefi\ApiBundle\Document\Client $client
-     * @return self
-     */
-    public function setClient(ClientInterface $client)
-    {
-        $this->client = $client;
-        return $this;
-    }
-
-    /**
-     * Get client
-     *
-     * @return Renatomefi\ApiBundle\Document\Client $client
-     */
-    public function getClient()
-    {
-        return $this->client;
-    }
-
-    /**
-     * Set user
-     *
-     * @param Renatomefi\UserBundle\Document\User $user
-     * @return self
-     */
-    public function setUser(UserInterface $user)
-    {
-        $this->user = $user;
-        return $this;
-    }
-
-    /**
-     * Get user
-     *
-     * @return Renatomefi\UserBundle\Document\User $user
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
 }
--- src/Renatomefi/ApiBundle/Document/RefreshToken.php
+++ src/Renatomefi/ApiBundle/Document/RefreshToken.php
@@ -3,83 +3,12 @@
 namespace Renatomefi\ApiBundle\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
-use FOS\OAuthServerBundle\Document\RefreshToken as BaseRefreshToken;
-use FOS\OAuthServerBundle\Model\ClientInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Class RefreshToken
  * @package Renatomefi\ApiBundle\Document
  * @ODM\Document
  */
-class RefreshToken extends BaseRefreshToken
+class RefreshToken extends Token
 {
-    /**
-     * @ODM\Id(strategy="auto")
-     */
-    protected $id;
-
-    /**
-     * @ODM\ReferenceOne(targetDocument="Client")
-     */
-    protected $client;
-
-    /**
-     * @ODM\ReferenceOne(targetDocument="Renatomefi\UserBundle\Document\User")
-     */
-    protected $user;
-
-    /**
-     * Get id
-     *
-     * @return id $id
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set client
-     *
-     * @param Renatomefi\ApiBundle\Document\Client $client
-     * @return self
-     */
-    public function setClient(ClientInterface $client)
-    {
-        $this->client = $client;
-        return $this;
-    }
-
-    /**
-     * Get client
-     *
-     * @return Renatomefi\ApiBundle\Document\Client $client
-     */
-    public function getClient()
-    {
-        return $this->client;
-    }
-
-    /**
-     * Set user
-     *
-     * @param Renatomefi\UserBundle\Document\User $user
-     * @return self
-     */
-    public function setUser(UserInterface $user)
-    {
-        $this->user = $user;
-        return $this;
-    }
-
-    /**
-     * Get user
-     *
-     * @return Renatomefi\UserBundle\Document\User $user
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
 }
