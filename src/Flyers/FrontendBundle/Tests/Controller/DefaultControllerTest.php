af60a96f5588442cbb6153580e40bcd3e622a5a4
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-03-20 16:32:06 -0300

    OAuth client meta tag tests

--- src/Flyers/FrontendBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Flyers/FrontendBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,6 +3,7 @@
 namespace Flyers\FrontendBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
 
 /**
  * Class DefaultControllerTest
@@ -12,6 +13,7 @@ class DefaultControllerTest extends WebTestCase
 {
     /**
      * Test if ng-app directive exists and is sammui
+     * @return \Symfony\Component\DomCrawler\Crawler
      */
     public function testAngularSammui()
     {
@@ -20,5 +22,22 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals('sammui', $crawler->filterXPath('//html')->attr('ng-app'));
+
+        return $crawler;
+    }
+
+    /**
+     * @depends testAngularSammui
+     * @param Crawler $crawler
+     */
+    public function testSammuiClient(Crawler $crawler)
+    {
+        $clientId = $crawler->filterXPath('//html/head/meta[@name="sammui-oauth2-client-id"]')->attr('content');
+        $clientSecret = $crawler->filterXPath('//html/head/meta[@name="sammui-oauth2-client-secret"]')->attr('content');
+
+        $this->assertNotNull($clientId);
+        $this->assertNotNull($clientSecret);
+        $this->assertTrue((is_string($clientId)), $clientId);
+        $this->assertTrue((is_string($clientSecret)), $clientSecret);
     }
 }
