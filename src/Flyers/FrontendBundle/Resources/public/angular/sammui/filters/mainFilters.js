556418f93181dd48fff11b622fec83ca3bf8fb85
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-03-20 14:45:13 -0300

    Cleaning up old project resources

--- src/Flyers/FrontendBundle/Resources/public/angular/sammui/filters/mainFilters.js
+++ src/Flyers/FrontendBundle/Resources/public/angular/sammui/filters/mainFilters.js
@@ -1,10 +1,4 @@
 'use strict';
 
 /* Filters */
-
-angular.module('sammui.mainFilters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+angular.module('sammui.mainFilters', []);
