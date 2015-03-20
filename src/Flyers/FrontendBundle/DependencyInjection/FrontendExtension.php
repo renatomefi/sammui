0333cb8894f7350592b2e33f9eb80c083b14ad7f
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-03-20 18:36:29 -0300

    Removing FrontEnd bundle dependency injection

--- src/Flyers/FrontendBundle/DependencyInjection/FrontendExtension.php
+++ src/Flyers/FrontendBundle/DependencyInjection/FrontendExtension.php
@@ -2,27 +2,13 @@
 
 namespace Flyers\FrontendBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Renatomefi\DependencyInjection\NoExtension;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ * Class FrontendExtension
+ * @package Flyers\FrontendBundle\DependencyInjection
  */
-class FrontendExtension extends Extension
+class FrontendExtension extends NoExtension
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
-    }
 }
