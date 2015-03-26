8c36b4b46d9d4ee92469970e9cb2cee33437b0bf
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-03-26 09:03:33 -0300

    FormBundle: Header directive based on type attribute

--- src/Renatomefi/FormBundle/Resources/public/angular/form/controllers/formControllers.js
+++ src/Renatomefi/FormBundle/Resources/public/angular/form/controllers/formControllers.js
@@ -146,8 +146,9 @@ angular.module('sammui.formControllers', ['ngRoute'])
     }])
     .controller('formFillingPagination', ['$scope', function ($scope) {
         $scope.templates = [
-            {name: 'users', url: '/bundles/form/angular/views/form/filling/partials/formFillingUser.html'},
-            {name: 'comments', url: '/bundles/form/angular/views/form/filling/partials/formFillingComment.html'},
+            {name: 'index', headerType: 'index', url: '/bundles/form/angular/views/form/filling/partials/index.html'},
+            {name: 'users', url: '/bundles/form/angular/views/form/filling/partials/user.html'},
+            {name: 'comments', url: '/bundles/form/angular/views/form/filling/partials/comment.html'},
             {name: 'Page 1', url: '/bundles/form/angular/views/form/pages/sammui_demo/1.html'},
             {name: 'Page 2', url: '/bundles/form/angular/views/form/pages/sammui_demo/2.html'}
         ];
--- src/Renatomefi/FormBundle/Resources/public/angular/form/directives/formDirectives.js
+++ src/Renatomefi/FormBundle/Resources/public/angular/form/directives/formDirectives.js
@@ -4,6 +4,10 @@ angular.module('sammui.formDirectives', [])
     .directive('formFillingHeader', function () {
         return {
             restrict: 'E',
+            scope: {
+                headerType: "@type",
+                protocol: '=protocol'
+            },
             templateUrl: '/bundles/form/angular/views/form/filling/partials/header.html'
         };
     });
