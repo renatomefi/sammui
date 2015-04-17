b87483707bee9b16c365c1b85c138bdf0a0d2940
Renato Mendes Figueiredo <gh@renatomefi.com.br>
2015-04-17 20:17:55 -0300

    Optimizing vagrant provision

--- bin/vagrant/provision.sh
+++ bin/vagrant/provision.sh
@@ -3,6 +3,7 @@
 echo "Starting sammui installation: " $(date)
 echo "--------------------------------------------------------"
 export COMPOSER_PROCESS_TIMEOUT=1200
+composer self-update
 composer create-project -n --keep-vcs renatomefidf/sammui /vagrant/sammui dev-master
 echo "Removing old and wrong vhosts:"
 sudo rm -rf /etc/nginx/sites-enabled/vhost-sammui*.conf
@@ -13,7 +14,9 @@ echo "Testing nginx configuration: " $([ "$?" == 0 ] && echo "ok" || echo "error
 echo "Finishing sammui installation: " $(date)
 echo "Checking installation:"
 php /vagrant/sammui/app/check.php
-echo "Restoring MongoDB for sammui:"
+echo "Creating MongoDB indexes and loading data"
+php /vagrant/sammui/app/console --env=dev doctrine:mongodb:schema:create --index
+php /vagrant/sammui/app/console --env=dev doctrine:fixtures:load --append
 mongorestore /vagrant/sammui/docs/mongo/master/
 echo "--------------------------------------------------------"
 echo "---------- End   ---------------------------------------"
